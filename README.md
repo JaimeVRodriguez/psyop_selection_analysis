37981e77547a568d4389ef96c4d4153e880847c4
JaimeVRodriguez <jaimerod26@gmail.com>
2023-01-30 15:18:10 -0500

    fix cleaning functions, remove nan/blanks

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # psyop_selection_analysis
 Exploratory and Statistical Analysis on PSYOP Assessment and Selection
+
+
+POAS - start: 2651 finish: 2633
+SFAS - start: 8347 finish: 8116
--- src/functions.py
+++ src/functions.py
@@ -12,15 +12,39 @@ def binary_language(df):
     df.LANG = (df.LANG.notna() & df.LANG.ne("")).astype(int)
     return df
 
+def drop_na_values(df):
+    df.dropna(how='any', inplace=True)
+    return df
+
+def flt_to_int(df, column):
+    df[column] = df[column].astype(int)
+    return df
+
+def clean_data(filepath, filenames, columnsB):
+
+    df = read_excel_files(filepath, filenames)
+    df = binary_language(df)
+    drop_na_values(df)
+    for i in columnsB:
+        df = flt_to_int(df, i)
+    return df
+
 
 
 if __name__ == '__main__':
     filepath = 'data/poas'
     filenames = ['poas16.xlsx', 'poas17.xlsx', 'poas18.xlsx', 'poas19.xlsx', 'poas20.xlsx', 'poas21.xlsx', 'poas22.xlsx']
+    filepath2 = 'data/sfas'
+    filenames2 = ['sfas16.xlsx', 'sfas17.xlsx', 'sfas18.xlsx', 'sfas19.xlsx', 'sfas20.xlsx', 'sfas21.xlsx', 'sfas22.xlsx']
+    
+    # poas = read_excel_files(filepath, filenames)
+    # # poas = binary_language(poas)
 
-    poas = read_excel_files(filepath, filenames)
+    # poas = clean_data(filepath, filenames, ['RACE', 'SEC', 'AB', 'RGR', 'PT', 'DEP', 'AGE', 'EL', 'SC', 'CO', 'FA', 'ST' ], ['DEP', 'PT', 'AGE', 'EL', 'SC', 'CO', 'FA', 'ST'] )
 
-    print(poas)
+    sfas = clean_data(filepath2, filenames2)
+    print(sfas)
+    sfas.to_excel('merged.xlsx', index=False)
 
 
 
