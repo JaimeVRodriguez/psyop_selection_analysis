26f28a299026232fc1d37e163dfe48c6abead335
JaimeVRodriguez <jaimerod26@gmail.com>
2023-01-30 16:56:15 -0500

    add age plots/adjust plot style

--- src/functions.py
+++ src/functions.py
@@ -39,11 +39,24 @@ def not_column_val(df, column, value):
     result = df[df[column] != value]
     return result
 
-def bar_plot(data, labels, title, ylabel):
+def select_bar_plot(data, labels, title, ylabel):
     fig, ax = plt.subplots()
     ax.bar(labels, data)
     ax.set_title(title, loc='left', size=18, fontweight='bold')
     ax.set_ylabel(ylabel)
+    for i, v in enumerate(data):
+        ax.text(i, v + 35, str(v), ha='center')
+
+def age_bar_plot(x, y, title, xlabel, ylabel, mean, high):
+    fig, ax = plt.subplots()
+    ax.bar(x, y)
+    ax.set_title(title, loc='left', size=18, fontweight='bold')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.axvline(mean, color='black', linestyle='--', linewidth=1, label=f'Average Age: {mean}')
+    ax.axvline(high, color='grey', linestyle='--', linewidth=1, label=f'Most Selected Age: {high}')
+    ax.legend()
+
 
 
 
